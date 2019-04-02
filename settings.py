5772afbaf4771b238cb1812a0cdbc19e04132b91
Rhaniel Magalhães <rhanielmag.xavier@gmail.com>
2019-04-02 20:46:15 -0300

    Atualização das variáveis de configuração

--- settings.py
+++ settings.py
@@ -1,3 +1,7 @@
+S3_BUCKET = 'toilter-s3-teste'
+S3_KEY = 'AKIAJTETGDHH4OAWSRKA'
+S3_SECRET = '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'
+
 database_auth = [
 	'instagromdb.cq6gkp7dzyjw.us-east-1.rds.amazonaws.com', #'db_end'
 	'InstagromADM', #'db_user'
