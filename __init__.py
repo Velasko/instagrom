118e80a9d5a15526b137566c0800a7cb5e8c24d0
Rhaniel Magalhães <rhanielmag.xavier@gmail.com>
2019-04-02 19:15:38 -0300

    Adição dos filtros

--- __init__.py
+++ __init__.py
@@ -1,11 +1,13 @@
 import flask
 import flask_login
+from .filters import datetimeformat
 
 app = flask.Flask(__name__)
 
 #generating a new secret key:
 #python -c 'import os; print(os.urandom(16))'
 app.secret_key = b'\xc4>#\xa1E\x1e\xa0a1\x93\xbe?s\xea\xbf\xf1'
+app.jinja_env.filters['datetimeformat'] = datetimeformat
 
 login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
