cb07cd28c9773c86c45678f6072b5772a6b4f35d
Rhaniel Magalhães <rhanielmag.xavier@gmail.com>
2019-04-02 19:15:18 -0300

    Alteração no feed

--- pages/feed.py
+++ pages/feed.py
@@ -10,6 +10,7 @@ def user_feed():
 	form = PostForm()
 
 	posts = db.get_posts()
+	likes = db.get_likes()
 
 	if form.validate_on_submit():
 		post = {'user': session['username'], 'image': form.content.data}
@@ -17,4 +18,27 @@ def user_feed():
 		return redirect(url_for('user_feed'))
 
 
-	return render_template('feed.html', form=form, posts=posts)
+	return render_template('feed.html', form=form, posts=posts, likes=likes)
+
+@app.route('/like/<user>/<int:post_id>', methods=['GET', 'POST'])
+def like(user, post_id):
+
+	likes = db.get_likes(post=post_id)
+
+	exists = False
+
+	for like in likes:
+		if like.user==user:
+			exists = True
+
+	if not exists:
+		new_like = {
+			'user': user,
+			'post': post_id
+		}
+
+		likes.append(**new_like)
+	else:
+		print("Like já existente!")
+
+	return redirect(url_for('user_feed'))
