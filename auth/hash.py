6aba4b998372b01bb407061d83bfd83db5cdb633
Luís Filipe Velasco da Silva <f.l.velasko@gmail.com>
2019-04-04 20:33:11 -0300

    Update hash.py

--- auth/hash.py
+++ auth/hash.py
@@ -2,5 +2,5 @@ import hashlib
 
 def passwd_hash(passwd):
 	sha_signature = \
-		hashlib.sha256(hash_string.encode()).hexdigest()
+		hashlib.sha256(passwd.encode()).hexdigest()
 	return sha_signature
