b98002d66e6d4deaebd988f55c033891243741b2
Luís Filipe Velasco da Silva <f.l.velasko@gmail.com>
2019-04-04 23:10:20 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,7 +4,6 @@ import flask_login
 from .filters import datetimeformat
 
 from flask_dynamo import Dynamo
-from flask_debugtoolbar import DebugToolbarExtension
 
 app = flask.Flask(__name__)
 
@@ -33,9 +32,6 @@ app.jinja_env.filters['datetimeformat'] = datetimeformat
 
 login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
-
-toolbar = DebugToolbarExtension(app)
-toolbar.init_app(app)
 
 from . import pages
 
